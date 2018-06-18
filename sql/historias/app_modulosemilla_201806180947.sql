2a8ead6a8c2dda1076a9c11d62b0a9bc8ef2b41f
davichoar <david.ascencios@pucp.pe>
2018-06-18 15:21:53 -0500

    historias dump

---
+++ sql/historias/app_modulosemilla_201806180947.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO public.app_modulosemilla (idmodulo,idzona,nombre,codigomodulojson,temperaturaideal,temperaturamin,temperaturamax,humedadtierraideal,humedadtierramin,humedadtierramax,humedadambienteideal,humedadambientemin,humedadambientemax,concentracionco2ideal,concentracionco2min,concentracionco2max,nivelaguaideal,nivelaguamin,nivelaguamax,filas,columnas,fechacreacion,habilitado,idusuarioauditado,condicionesshidas) VALUES 
+(1,1,'Modulo 1',1,20,17,23,77,70,88,70,40,78,50,46,57,60,47,65,8,8,'2018-05-29 00:00:00.000',true,1,NULL)
+,(2,1,'Modulo 2',2,20,17,23,77,70,88,70,40,78,50,46,57,60,47,65,8,8,'2018-05-29 00:00:00.000',true,1,NULL)
+,(3,1,'Modulo 3',3,20,17,23,77,70,88,70,40,78,50,46,57,60,47,65,8,8,'2018-05-29 00:00:00.000',true,1,NULL)
+,(4,4,'Modulo 1',4,20,17,23,77,70,88,70,40,78,50,46,57,60,47,65,8,8,'2018-05-29 00:00:00.000',true,1,false)
+,(5,4,'Modulo 2',5,20,17,23,77,70,88,70,40,78,50,46,57,60,47,65,8,8,'2018-05-29 00:00:00.000',true,1,NULL)
+;
---
+++ sql/historias/app_zona_201806180946.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO public.app_zona (idzona,idtipozona,idinvernadero,codigozonajson,nombre,area,temperaturaideal,temperaturamin,temperaturamax,phideal,phmin,phmax,concentracionco2ideal,concentracionco2min,concentracionco2max,fechacreacion,habilitado,idusuarioauditado,condicionesshidas) VALUES 
+(1,1,1,1,'Zona 1',34,25,20,30,NULL,5,8,20,10,30,'2018-05-28 00:00:00.000',true,1,NULL)
+,(2,2,1,2,'Zona 2',50,20,14,26,6.5,6.2,7.4,40,30,50,'2018-05-29 00:00:00.000',true,1,false)
+,(3,2,1,3,'Zona 3',40,22,20,23,7,6,8,49,35,55,'2018-05-30 00:00:00.000',true,1,false)
+,(4,1,1,4,'Zona 4',40,22,20,23,7,6,8,49,35,55,'2018-05-31 00:00:00.000',true,1,false)
+;
